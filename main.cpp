cc7bb8e5c7fd05cc263723b8f1aa54d5e26642a4
DARKREDHAYABUSA <johnathon.licon@my.tccd.edu>
2020-02-27 02:18:03 +0000

    Task 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,52 @@
+//a program to practice passing a struct by pointer (*)
 #include <iostream>
+#include <string>
+using namespace std;
+//declartion of the struct
+struct car
+{
+  string Make;
+  string Model;
+  int Year;
+  string Color;
+};
+//function defintion declared above int main()
+void printCar(car *c)
+{
+ cout << "\nCar details: "<< endl;
+ cout << "Make - " << c->Make << endl;
+ cout << "Model - " << c->Model << endl;
+ cout << "Year - " << c->Year << endl;
+ //set the color variable based upon the year, and print it
+ if (c->Year <= 2010)
+{
+  c->Color = "RED";
+}
+else if (c->Year > 2010)
+{
+  c->Color = "GREEN";
+}
+//print the color to the screen
+cout << "Color: " << c->Color << endl;
+}
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+//declare an instance of the struct
+car car1;
+car1.Make = "Hyundai";
+car1.Model = "Genesis";
+car1.Year = 2016;
+
+car car2 = {"GMC", "Yukon", 1975};
+
+car car3 = {"Toyota", "Avalon", 2003};
+
+//Print each car
+printCar (&car1);
+printCar (&car2);
+printCar (&car3);
+
+
+  return 0;
 }
